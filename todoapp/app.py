2716565ba5809d324fff63254964e443711bd37b
cavilosa <cavilosa33@gmail.com>
2021-03-24 10:23:13 -0600

    Established db.relationship and db.foreigKey relstions on created todolists(parent table) and todos(child)

--- todoapp/app.py
+++ todoapp/app.py
@@ -17,11 +17,20 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'),
+                nullable=False)
 
     def __rep__(self):
         return f"<Todo {self.id} {self.descriptions}>"
 
 #db.create_all() - migrations will do it for us?
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
 
 @app.route("/")
 def index():
