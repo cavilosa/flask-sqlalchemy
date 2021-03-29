539346c22b2e3dba43e9246bedec7ba849bb3b5a
cavilosa <cavilosa33@gmail.com>
2021-03-29 14:05:37 -0600

    many-to-many relationship

--- todoapp/app.py
+++ todoapp/app.py
@@ -11,6 +11,21 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+order_items = db.Table('order_items',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
+class Order(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  status = db.Column(db.String(), nullable=False)
+  products = db.relationship('Product', secondary=order_items,
+      backref=db.backref('orders', lazy=True))
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
